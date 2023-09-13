7dc21b7edd36dff28ac0a867decbf56fcf5f850e
ijikeman <keiji.ue@gmail.com>
2023-09-13 14:06:47 +0900

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -12,15 +12,23 @@ enum layer_names {
 };
 
 enum custom_keycodes {
+// LAYER
     QWERTY = SAFE_RANGE,
     LOWER,
     RAISE,
     ADJUST,
-    M_LANG = SAFE_RANGE,
+
+// MACRO
+    M_LANG,
     M_BRC,
     M_QUOT,
     M_MINUS,
     M_S_MINUS,
+
+// WINDOWS_LAYER
+    WIN_TAB,
+    WIN_LEFT,
+    WIN_RGHT,
 };
 
 #define LOWER  MO(_LOWER)
@@ -30,49 +38,49 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
 * ,-----------------------------------------------------------------------------------.
-* | TAB |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [/] |
+* | ESC  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [/] |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
-* | Ctr |  A   |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
-* |------+------+------+------+------+-----|------+------+------+------+------+------|
-* | Sft |  Z   |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /  | '/\   |
+* |TAB/CTL|  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | '/\  |
+* |------+------+------+------+------+------|------+------+------+------+------+------|
+* |SFT/CAP|  Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  /   | ENTER|
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* | ESC | IME |  ALT | GUI  |Mouse|Spc/Lower|Bs/Raise|   | LEFT | DOWN |  UP  | RIGHT |
+* |      |      |      | ALT  |GUI/ADJ|SPC/LOW|BS/RAI| IME| ALT  |      |      |      |
 * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
-   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, M_QUOT,
-   KC_ESC,  M_LANG, KC_LALT, KC_LGUI, ADJUST, LT(_LOWER,KC_SPC),LT(_RAISE, KC_BSPC),KC_NO, KC_NO, KC_NO, KC_NO,KC_NO
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
+  LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
+  LSFT_T(KC_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  KC_NO, KC_NO, KC_NO, KC_LALT, LT(ADJUST,KC_LGUI), LT(_LOWER,KC_SPC),LT(_RAISE, KC_BSPC),M_LANG, KC_RALT, KC_NO, KC_NO,KC_NO
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  _/+ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | CAPS |      |      |      |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
+ * |      |      |      |      |      |      | LEFT | DOWN |  UP  | RGHT |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      | WIN_L|WIN_T |WIN_T |WIN_R |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Reset|      |      |      |      |      | Del  |      |      |      |      |      |
+ * | RESET|      |      |      |      |      | DEL  |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
   S(KC_GRV),S(KC_1),S(KC_2), S(KC_3), S(KC_4),S(KC_5),S(KC_6), S(KC_7), S(KC_8), S(KC_9),S(KC_0), M_S_MINUS,
-  KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   _______, _______,
+  _______, _______, _______, _______, _______, _______, WIN_LEFT,WIN_TAB,WIN_TAB, WIN_RGHT, _______, _______,
   QK_BOOT, _______, _______, _______, _______, _______, KC_DEL, _______, _______, _______, _______, _______
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | -/=  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | CAPS |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 | F12  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | RESET|      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |      |      |      |      |      |      |      |      |      |      |      | RESET|
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
@@ -84,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -110,6 +118,34 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 static uint16_t pressed_time = 0; // キーが押されてからの時間
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+    case WIN_TAB:
+        if (record->event.pressed) {
+            register_code(KC_LGUI);
+            register_code(KC_TAB);
+            unregister_code(KC_TAB);
+            unregister_code(KC_LGUI);
+        }
+        break;
+    case WIN_LEFT:
+        if (record->event.pressed) {
+            register_code(KC_LGUI);
+            register_code(KC_LCTL);
+            register_code(KC_LEFT);
+            unregister_code(KC_LEFT);
+            unregister_code(KC_LCTL);
+            unregister_code(KC_LGUI);
+        }
+        break;
+    case WIN_RGHT:
+        if (record->event.pressed) {
+            register_code(KC_LGUI);
+            register_code(KC_LCTL);
+            register_code(KC_RGHT);
+            unregister_code(KC_RGHT);
+            unregister_code(KC_LCTL);
+            unregister_code(KC_LGUI);
+        }
+        break;
     case M_LANG: // 日本語切り替え
         if (record->event.pressed) {
             register_code(KC_LALT);
