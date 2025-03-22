ac7371432f58c37ba73a1f6fd6edee560b348969
ijikeman <keiji.ue@gmail.com>
2025-03-22 22:19:03 +0900

    w

--- build.sh
+++ build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 QMK_VERSION='0.28.0'
 QMK_NAME='qmk_firmware'
-HOMEDIR='/home/vscode/project/'
-REPOSITORY_NAME='qmk'
-
-# HOMEDIRをEscapeしてESCAPE_HOMEDIRに代入する
-ESCAPE_HOMEDIR=$(echo $HOMEDIR | sed 's/\//\\\//g')
 
 # 第一引数はキーボード名
 args_kb=$1
-args_km=$2
-
-# args_kbを/で分割して、最初の要素を取得しkeyboard_nameに代入
-keyboard_name=$(echo $args_kb | cut -d'/' -f1)
+args_rev=$2
+args_km=$3
 
 # qmk_firmware(https://github.com/qmk/qmk_firmware.git)をlatestのみをクローン
 # もしqmk_firmwareが存在する場合はスキップ
@@ -27,7 +20,7 @@ if [ ! -d "qmk_firmware" ]; then
 fi
 
 # keyboardsの対象フォルダを入れ替え
-rm -rf ${QMK_NAME}/keyboards/${keyboard_name}
+rm -rf ${QMK_NAME}/keyboards/${args_kb}
 cp -Rp keyboards ${QMK_NAME}/keyboards
 
 # qmk_firmwareに移動してコンパイル
@@ -39,4 +32,4 @@ cd qmk_firmware
 # fi
 
 # build hex
-bash util/docker_build.sh ${args_kb}:${args_km}
+bash util/docker_build.sh ${args_kb}/${args_rev}:${args_km}
