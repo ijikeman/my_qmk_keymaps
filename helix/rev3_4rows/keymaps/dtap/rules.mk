f02c5dab93a6aff086bc2dcdaacb95a433f8f78e
ijikeman <keiji.ue@gmail.com>
2025-03-22 23:25:11 +0900

    typo rename to glcdfont.c

--- helix/rev3_4rows/keymaps/dtap/rules.mk
+++ helix/rev3_4rows/keymaps/dtap/rules.mk
@@ -1,4 +1,2 @@
-DEFAULT_FOLDER = helix/rev2
-
 HELIX_TOP_DIR := $(patsubst %/,%,$(dir $(lastword $(MAKEFILE_LIST))))
 TAP_DANCE_ENABLE = yes
--- lets_split/rev2/rules.mk
+++ lets_split/rev2/rules.mk
@@ -1,2 +1,8 @@
 # Disable unsupported hardware
 AUDIO_SUPPORTED = no
+
+# keyboard.jsonと合わせる
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+COMMAND_ENABLE = yes
