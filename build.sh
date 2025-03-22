a7741740d27dc8daa66a68a79925bb94dde8b0f3
ijikeman <keiji.ue@gmail.com>
2025-03-22 10:12:10 +0900

    fixed

--- build.sh
+++ build.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 QMK_VERSION='0.22.0'
+QMK_NAME='qmk_firmware'
+HOMEDIR='/home/vscode/project/'
+REPOSITORY_NAME='qmk'
+
+# HOMEDIRをEscapeする
+ESCAPE_HOMEDIR=``
 
 # 第一引数はキーボード名
-keyboard=$1
-build=$2
-folder=$3
+args_kb=$1
+args_km=$2
 
 # qmk_firmware(https://github.com/qmk/qmk_firmware.git)をlatestのみをクローン
 # もしqmk_firmwareが存在する場合はスキップ
 if [ ! -d "qmk_firmware" ]; then
     git clone -b ${QMK_VERSION} --depth 1 --recurse-submodules https://github.com/qmk/qmk_firmware.git
-    # vscode-serverの場合はディレクトリパスが変わる為、コメントアウトする
-    sed -i -e 's/^qmk_userspace_dir=""/qmk_userspace_dir=""\nqmk_firmware_dir="\/home\/vscode\/project\/qmk\/qmk_firmware\/"/' qmk_firmware/util/docker_cmd.sh
 fi
+# vscode-serverの場合はディレクトリパスが変わる為、コメントアウトする
+sed -i -e 's/^qmk_userspace_dir=""/qmk_userspace_dir=""\nqmk_firmware_dir="${ESCAPE_HOMEDIR}\/${REPOSITORY_NAME}\/qmk_firmware\/"/' qmk_firmware/util/docker_cmd.sh
 
-# 引数のフォルダにrules.mkを上書き
-#cp rules.mk qmk_firmware/keyboards/$1
+# 引数のフォルダを入れ替え
+rm -rm ${QMK_NAME}/keyboards/${args_kd}
 
 # キーマップのフォルダにoriginalフォルダを作成
-#if [ ! -d "qmk_firmware/keyboards/$keyboard/keymaps/original" ]; then
-#    mkdir qmk_firmware/keyboards/$keyboard/keymaps/original
-#fi
-
-# originalフォルダにキーボード名.keymap.cをコピー
-#cp $keyboard.keymap.c qmk_firmware/keyboards/$keyboard/keymaps/original/keymap.c
-# config.hをコピー
-#cp $keyboard.config.h qmk_firmware/keyboards/$keyboard/keymaps/original/config.h
 
 # qmk_firmwareに移動
 cd qmk_firmware
-#util/docker_build.sh $1:default
+util/docker_build.sh ${args_kb}:${args_km}
