cc7f27b89584a8709b40dca871f5d2102d881903
ijikeman <keiji.ue@gmail.com>
2025-04-01 22:08:31 +0900

    fixed lets

--- keyboards/lets_split/keymaps/macro/keymap.c
+++ keyboards/lets_split/keymaps/macro/keymap.c
@@ -13,14 +13,15 @@ enum layer_names {
 
 #define LOWER  LT(_LOWER, KC_SPC)
 #define RAISE  LT(_RAISE, KC_BSPC)
-#define ADJUST MO(_ADJUST)
+// #define ADJUST MO(_ADJUST)
 #define MLCTL  LCTL_T(KC_TAB)
 #define MLANG1 LALT(KC_GRV)
 #define MLANG2 LCTL(KC_SPC)
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-    M_BRC = SAFE_RANGE,
+    ADJUST = SAFE_RANGE,
+    M_BRC,
     M_MINUS,
     M_QUOT,
     M_S_MINUS,
@@ -42,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
-  MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_CAPS, KC_NO,  KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE,  MLANG1,     ADJUST,  KC_NO,   KC_NO,   KC_NO
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   M_BRC,
+  MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L  KC_SCLN,  M_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+  KC_CAPS, KC_NO,  KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE,  MLANG1,  ADJUST,   KC_NO,   KC_NO,   KC_NO
 ),
 
 /* Lower
@@ -139,7 +140,16 @@ static void user_mt(keyrecord_t *record, uint16_t modcode, uint16_t keycode, uin
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    // Window切り替え機能(on Windows)
+        case ADJUST:
+        if (record->event.pressed) {
+          layer_on(_LOWER);
+          layer_on(_RAISE);
+        } else {
+          layer_off(_LOWER);
+          layer_off(_RAISE);
+        }
+        break;
+        // Window切り替え機能(on Windows)
     case WIN_TAB:
         if (record->event.pressed) {
             register_code(KC_LGUI);
