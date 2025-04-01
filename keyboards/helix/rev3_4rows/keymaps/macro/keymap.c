e1fdc5742e2b9b6f8f0f6a2a3657fc70c900e325
ijikeman <keiji.ue@gmail.com>
2025-04-01 22:06:33 +0900

    fixed

--- keyboards/helix/rev3_4rows/keymaps/macro/keymap.c
+++ keyboards/helix/rev3_4rows/keymaps/macro/keymap.c
@@ -26,11 +26,7 @@ enum layer_names {
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-  EISU = SAFE_RANGE,
-  KANA,
-  ADJUST,
-  RGBRST
-
+  ADJUST = SAFE_RANGE
 // MACRO
   ,
   M_BRC,
@@ -65,12 +61,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | CAPS |      |      | ALT  |  GUI | LOWER|Space |Space | RAISE| LANG |ADJUST|      |      |      |
    * `-------------------------------------------------------------------------------------------------'
    */
-[_QWERTY] = LAYOUT(
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
-  MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_CAPS, KC_NO,  KC_NO, KC_LALT, KC_LGUI,LOWER,KC_SPC,KC_SPC,RAISE,MLANG1,ADJUST, KC_NO,  KC_NO,   KC_NO
-),
+  [_QWERTY] = LAYOUT(
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
+      MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+      KC_CAPS,KC_NO,   KC_NO, KC_LALT, KC_LGUI,   LOWER,    KC_SPC,  KC_SPC, RAISE,  MLANG1,  ADJUST,   KC_NO,    KC_NO,   KC_NO
+      ),
+
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  _/+ |
@@ -85,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, M_S_MINUS,
       _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, MS_WHLD, MS_WHLU,
-      _______, _______,WIN_LEFT, WIN_TAB, WIN_RGHT, _______,                  MS_LEFT, MS_DOWN,  MS_UP,  MS_RGHT, MS_BTN1, MS_BTN2,
+      _______, _______,WIN_LEFT, WIN_TAB,WIN_RGHT, _______,                   MS_LEFT, MS_DOWN,  MS_UP,  MS_RGHT, MS_BTN1, MS_BTN2,
       _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, QK_BOOT
       ),
 
@@ -119,29 +116,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  LAYOUT(
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
       )
+
 };
 
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* Left side encoder */
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
-        }
-    } else if (index == 1) { /* Right side encoder */
-        if (clockwise) {
-            tap_code(KC_DOWN);
-        } else {
-            tap_code(KC_UP);
-        }
-    }
-    return true;
-}
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -177,30 +159,6 @@ static void user_mt(keyrecord_t *record, uint16_t modcode, uint16_t keycode, uin
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case EISU:
-      if (record->event.pressed) {
-        if (is_mac_mode()) {
-          register_code(KC_LNG2);
-        }else{
-          tap_code16(LALT(KC_GRAVE));
-        }
-      } else {
-        unregister_code(KC_LNG2);
-      }
-      return false;
-      break;
-    case KANA:
-      if (record->event.pressed) {
-        if (is_mac_mode()) {
-          register_code(KC_LNG1);
-        }else{
-          tap_code16(LALT(KC_GRAVE));
-        }
-      } else {
-        unregister_code(KC_LNG1);
-      }
-      return false;
-      break;
     case ADJUST:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -210,15 +168,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_RAISE);
       }
       break;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-        }
-      #endif
-      break;
-
     // Original Macro
     case WIN_TAB:
         if (record->event.pressed) {
--- keyboards/helix/rev3_4rows/keymaps/macro2/config.h
+++
@@ -1,2 +0,0 @@
-#define TAPPING_TERM 120 // 長押しによる切りMacroキー替え時間をmsで指定
-#define DEBOUNCE 20 // チャタリング解消確認中
