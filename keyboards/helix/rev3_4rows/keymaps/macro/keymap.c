073caf1174adc3ad52c6860a516ac88f7fa816b4
ijikeman <keiji.ue@gmail.com>
2026-01-09 12:12:03 +0900

    fixed

--- keyboards/helix/rev3_4rows/keymaps/macro/keymap.c
+++ keyboards/helix/rev3_4rows/keymaps/macro/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |Tab/Caps| Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  [/] |
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  [/] |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  | '/\  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT(
-      M_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
+      KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
       MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
       KC_NO,  KC_NO,   KC_NO, KC_LALT, KC_LGUI,   LOWER,    KC_SPC,  KC_SPC, RAISE,  MLANG1,  ADJUST,   KC_NO,    KC_NO,   KC_NO
@@ -72,18 +72,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |  _/+ |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             | LEFT | DOWN |  UP  | RGHT |WHEELD|WHEELU|
+   * | Caps |      | WIN_L| WIN_T| WIN_R|      |             | LEFT | DOWN |  UP  | RGHT |WHEELD|WHEELU|
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      | WIN_L| WIN_T| WIN_R|      |             |M-LEFT|M-DOWN| M-UP |M-RGHT|L-CLCK|R-CLCK|
+   * |      |      |      |      |      |      |             |M-LEFT|M-DOWN| M-UP |M-RGHT|L-CLCK|R-CLCK|
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      | DEL  |      |      |      |      |RESET |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT(
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, M_S_MINUS,
-      _______, _______,WIN_LEFT, WIN_TAB,WIN_RGHT, _______,                   KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, MS_WHLD, MS_WHLU,
-      _______, _______, _______, _______, _______, _______,                   MS_LEFT, MS_DOWN,  MS_UP,  MS_RGHT, MS_BTN1, MS_BTN2,
-      _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, QK_BOOT
+      KC_CAPS, KC_NO, WIN_LEFT, WIN_TAB,WIN_RGHT, KC_NO,                    KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, MS_WHLD, MS_WHLU,
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    MS_LEFT, MS_DOWN,  MS_UP,  MS_RGHT, MS_BTN1, MS_BTN2,
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_DEL,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  QK_BOOT
       ),
 
   /* Raise
@@ -99,9 +99,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT(
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    M_MINUS,
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
       ),
 
   /* Adjust (Lower + Raise)
@@ -117,9 +117,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ADJUST] =  LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
+      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
       )
 };
 
