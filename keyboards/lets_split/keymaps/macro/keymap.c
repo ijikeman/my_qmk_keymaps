d754b4a49dddee8c44113d6c481a4e36f337471d
ijikeman <keiji.ue@gmail.com>
2026-01-09 12:24:29 +0900

    fixed

--- keyboards/lets_split/keymaps/macro/keymap.c
+++ keyboards/lets_split/keymaps/macro/keymap.c
@@ -34,7 +34,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* QWERTY
 * ,-----------------------------------------------------------------------------------.
-* | ESC  | Q    |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [/] |
+* | ESC  | Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  [/] |
 * |------+------+------+------+------+-------------+------+------+------+------+------|
 * |TAB/CTL|  A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | '/\  |
 * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
   MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_NO,  KC_NO,   KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE,  MLANG1,     ADJUST,  KC_NO,   KC_NO,   KC_NO
+  KC_NO,  KC_NO,  KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE,  MLANG1,     ADJUST,  KC_NO,   KC_NO,   KC_NO
 ),
 
 /* Lower
@@ -62,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,KC_CIRC,KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, M_S_MINUS,
-  KC_CAPS,  KC_NO, WIN_LEFT, WIN_TAB, WIN_RGHT,KC_NO,  KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, MS_WHLD, MS_WHLU,
-  KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,  MS_LEFT, MS_DOWN, MS_UP,  MS_RGHT, MS_BTN1, MS_BTN2,
-  KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_DEL,  KC_NO,   KC_NO,  KC_NO,   KC_NO,   QK_BOOT 
+  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, M_S_MINUS,
+  KC_CAPS, _______,WIN_LEFT,WIN_TAB, WIN_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, MS_WHLD, MS_WHLU,
+  _______, _______, _______, _______, _______, _______, MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, MS_BTN1, MS_BTN2,
+  _______, _______, _______, _______, _______, _______, KC_DEL, _______, _______, _______, _______, QK_BOOT 
 ),
 
 /* Raise
@@ -80,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-  KC_GRV,  KC_1,  KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   M_MINUS,
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   M_MINUS,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] =  LAYOUT_ortho_4x12(
-  KC_F1,  KC_F2,  KC_F3   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_F11, KC_F12,
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, 
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
 
