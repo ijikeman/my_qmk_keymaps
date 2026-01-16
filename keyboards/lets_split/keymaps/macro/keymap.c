7d359b6b13aa03a753d18b509be916e8f8367709
ijikeman <keiji.ue@gmail.com>
2026-01-16 15:45:02 +0900

    fixed

--- keyboards/lets_split/keymaps/macro/keymap.c
+++ keyboards/lets_split/keymaps/macro/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* | Caps |      |      | ALT  |  GUI | LOWER| RAISE|LANG/CAPS|ADJUST|      |      |      |
+* |      |      |      | ALT  |  GUI | LOWER| RAISE|LANG/CAPS|ADJUST|      |      |      |
 * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
@@ -204,7 +204,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         user_mt(record, KC_ESC, KC_CAPS, 0, &pressed, &pressed_time);
         return false;
         break;
-    }
     // 長押しマクロ(LANG or CAPS)
     case M_LANG_CAPS:
         user_mt(record, LALT(KC_GRV), KC_CAPS, 0, &pressed, &pressed_time);
