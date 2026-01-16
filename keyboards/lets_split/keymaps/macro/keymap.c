8636908c71bcbab168167c0edd0dbefa62930e1d
ijikeman <keiji.ue@gmail.com>
2026-01-16 15:41:00 +0900

    fixed

--- keyboards/lets_split/keymaps/macro/keymap.c
+++ keyboards/lets_split/keymaps/macro/keymap.c
@@ -26,6 +26,7 @@ enum custom_keycodes {
     M_QUOT,
     M_S_MINUS,
     M_ESC,
+    M_LANG_CAPS,
     WIN_TAB,
     WIN_LEFT,
     WIN_RGHT,
@@ -40,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |------+------+------+------+------+------|------+------+------+------+------+------|
 * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 * |------+------+------+------+------+------+------+------+------+------+------+------|
-* | Caps |      |      | ALT  |  GUI | LOWER| RAISE| LANG |ADJUST|      |      |      |
+* | Caps |      |      | ALT  |  GUI | LOWER| RAISE|LANG/CAPS|ADJUST|      |      |      |
 * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
   KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    M_BRC,
   MLCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, M_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-  KC_CAPS,  KC_NO,  KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE,    MLANG1,    ADJUST,  KC_NO,   KC_NO,   KC_NO
+  KC_NO,  KC_NO,  KC_NO, KC_LALT, KC_LGUI,   LOWER,   RAISE, M_LANG_CAPS,    ADJUST,  KC_NO,   KC_NO,   KC_NO
 ),
 
 /* Lower
@@ -201,6 +202,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // 長押しマクロ(ESC or CAPS)
     case M_ESC:
         user_mt(record, KC_ESC, KC_CAPS, 0, &pressed, &pressed_time);
+        return false;
+        break;
+    }
+    // 長押しマクロ(LANG or CAPS)
+    case M_LANG_CAPS:
+        user_mt(record, LALT(KC_GRV), KC_CAPS, 0, &pressed, &pressed_time);
         return false;
         break;
     }
